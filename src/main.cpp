eaab6cb8355ac7eff6f9233208d76cfea5fe4a59
JoeLee3511 <zyngpan@gmail.com>
2025-08-02 11:25:16 -0400

    added code to filter out individual columns

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <map>
 #include <fstream>
+#include <sstream>
 #include "Graph.h"
 using namespace std;
 
@@ -12,5 +13,25 @@ int main() {
         cout << "Error opening file" << endl;
         return 2;
     }
+
+    // reads the header and prints it
+    string row1;
+    getline(dataset, row1);
+    cout << row1 << endl;
+
+    // reads the selected column and prints it
+    string line;
+    while (getline(dataset, line)) {
+        stringstream ss(line);
+        string column;
+        int columnIndex = 0;
+        while (getline(ss, column, ',')) {
+            if (columnIndex == 4) {   // change the number to select a different column
+                cout << column << endl;
+            }
+            columnIndex++;
+        }
+    }
+
 
 }
